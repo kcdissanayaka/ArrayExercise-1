3812cc8cdd128b705b2790300ca9511db3dff956
kcdissanayaka <kc.dissanayaka@outlook.com>
2023-10-02 03:56:31 +0300

    ArrayQuis1Creating an array and print Values

---
+++ src/Exsersice1.java
@@ -0,0 +1,15 @@
+
+public class Exsersice1 {
+
+	public static void main(String[] args) {
+		String [] myStringArray = {"Green","Blue","Yellow"};
+		
+		//System.out.println(myStringArray[1]);
+		
+		for (int i = 0; i< myStringArray.length; i++) {
+			
+			System.out.println(myStringArray[i]);
+		}
+	}
+
+}
